fca4770aa1e0f2d66836cef3614f9d0ad83bbf1a
DHX98 <haox.dai@gmail.com>
2022-06-04 22:46:14 -0700

    SignUp component added

--- AppDev/frontend/src/index.jsx
+++ AppDev/frontend/src/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import SignUp from './component/SignUp';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+ReactDOM.render(<SignUp />, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
