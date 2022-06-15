4aef0310f29594b5753d4ae7f252c1c34b844944
distretto <vincentli0510@gmail.com>
2022-06-15 19:43:27 -0400

    Video Upload

--- AppDev/backend/videos/views.py
+++ AppDev/backend/videos/views.py
@@ -1,5 +1,26 @@
-from rest_framework.views import APIView
-from .models import Video
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render
 
-class VideoView(APIView):
-    pass
+from .models import Upload, UploadPrivate
+
+
+def video_upload(request):
+    if request.method == 'POST':
+        video_file = request.FILES['image_file']
+        video_type = request.POST['image_type']
+        if settings.USE_S3:
+            if video_type == 'private':
+                upload = UploadPrivate(file=video_file)
+            else:
+                upload = Upload(file=video_file)
+            upload.save()
+            image_url = upload.file.url
+        else:
+            fs = FileSystemStorage()
+            filename = fs.save(video_file.name, video_file)
+            image_url = fs.url(filename)
+        return render(request, 'upload.html', {
+            'image_url': image_url
+        })
+    return render(request, 'upload.html')
