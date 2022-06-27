bb6aa29d55de314f8e2e056ad0a4bd085d6ad1cf
distretto <vincentli0510@gmail.com>
2022-06-27 00:27:50 -0400

    S3 Access and Bucket Update

--- AppDev/backend/backend/settings.py
+++ AppDev/backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'profiles',
     'videos',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -160,3 +161,22 @@ EMAIL_HOST_USER = "streamsocket@outlook.com"
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
 EMAIL_FROM = "streamsocket@outlook.com"
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'mysite/static'),
+# ]
+
+AWS_ACCESS_KEY_ID = 'AKIAXQ3TTZO3D5HFNG4L'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'streamsocket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
--- AppDev/backend/videos/views.py
+++ AppDev/backend/videos/views.py
@@ -4,3 +4,20 @@ from django.core.files.storage import FileSystemStorage
 
 def video_upload(request):
     pass
+# from django.contrib.auth.decorators import login_required
+# from django.views.generic.edit import CreateView
+# from django.urls import reverse_lazy
+#
+# from .models import Document
+#
+#
+# class DocumentCreateView(CreateView):
+#     model = Document
+#     fields = ['upload', ]
+#     success_url = reverse_lazy('home')
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         documents = Document.objects.all()
+#         context['documents'] = documents
+#         return context
