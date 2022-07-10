c4ed4b839becade70fd8b2a7d6e08e9f7b0111ec
Tejas <tsrivatsav@berkeley.edu>
2022-07-10 13:11:18 -0400

    wav2vec2 model inference supported

---
+++ ML/wav2vec2_pipeline/split_helper.py
@@ -0,0 +1,35 @@
+### split a (string) sentence into (array of strings) smaller phrases
+def split_phrases(text, char_limit):
+    text_length = len(text)
+    output_arr = []
+    c = 0
+    while c < text_length:
+        if text_length - c > char_limit:
+            offset = c + char_limit
+            ### find end of last completed word
+            while text[offset] != ' ':
+                offset -= 1
+                ### if transcribed word is longer than char_limit, override and just output it
+                if offset < c:
+                    offset = c + char_limit
+                    break
+            output_arr.append(text[c:offset])
+            c = offset + 1
+        else:
+            output_arr.append(text[c:])
+            break
+    return output_arr
+
+### split a start, end segment into (array of [start, end]) smaller segments
+def split_segments(start, end, periodic_limit):
+    segment_length = end - start
+    output_arr = []
+    l = 0
+    while l < segment_length:
+        if segment_length - l > periodic_limit:
+            output_arr.append([l, l + periodic_limit])
+            l += periodic_limit
+        else:
+            output_arr.append([l, end])
+            break
+    return output_arr
---
+++ ML/wav2vec2_pipeline/wav2vec2_inference.py
@@ -0,0 +1,126 @@
+from pyAudioAnalysis import audioSegmentation as aS
+from convert_time_format import convert_time_format
+import getopt
+import librosa
+import noisereduce as nr
+from split_helper import split_phrases, split_segments
+import sys
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+
+def main(argv):
+    ### read command line arguments
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:c:p",["ifile=","ofile=","char_limit=", "periodic_limit="])
+    except getopt.GetoptError:
+        print('python wav2vec2_inference.py -i <inputfile> -o <outputfile> -c char_limit -p periodic_limit')
+        sys.exit(2)
+    input_filepath, output_filepath, char_limit, periodic_limit = '', '', 40, 4
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Usage: python wav2vec2_inference.py -i <inputfile> -o <outputfile> -c char_limit -p periodic_limit')
+            sys.exit()
+        ### input audio filepath (wav or flac)
+        elif opt in ("-i", "--ifile"):
+            input_filepath = arg
+        ### output srt filepath
+        elif opt in ("-o", "--ofile"):
+            output_filepath = arg
+        ### character limit per line for captioning
+        elif opt in ("-c", "--char_limit"):
+            char_limit = arg
+        ### time limit per phrase (in seconds) for captioning
+        elif opt in ("-p", "--periodic_limit"):
+            periodic_limit = arg
+
+    # ### input audio filepath (wav or flac)
+    # input_filepath = 'noisy_testset_wav\p257_023.wav'
+    # ### output srt filepath
+    # output_filepath = 'transcription_output.srt.txt'
+    # ### character limit per line for captioning
+    # char_limit = 40
+    # ### time limit per phrase (in seconds) for captioning
+    # periodic_limit = 4
+
+    ### check to make sure input and output files are valid
+    if input_filepath == '':
+        raise FileNotFoundError('No file provided as input. Please use -i <inputfile> as part of command-line arguments')
+    if output_filepath == '':
+        raise FileNotFoundError('No file provided as output. Please use -o <outputfile> as part of command-line arguments')
+
+    ### load flac or wav file
+    try:
+        audio, sr = librosa.load(input_filepath, sr=16000)
+    except FileNotFoundError:
+        print('Unable to find input file path:', input_filepath)
+        sys.exit()
+
+    ### wav2vec2 model
+    tokenizer_name = 'facebook/wav2vec2-large-960h'
+    model_name = 'facebook/wav2vec2-large-960h'
+    tokenizer = Wav2Vec2Tokenizer.from_pretrained(tokenizer_name)
+    model = Wav2Vec2ForCTC.from_pretrained(model_name)
+
+    # https://github.com/tyiannak/pyAudioAnalysis/blob/944f1d777bc96717d2793f257c3b36b1acf1713a/pyAudioAnalysis/audioSegmentation.py#L670
+    ### segment limits
+    segmentLimits = aS.silence_removal(audio, sr, 0.05, 0.05, 1.0, 0.2)
+
+    ### subdivide segments further based on periodic limit
+    periodic_split = []
+    for start, end in segmentLimits:
+        if end - start < periodic_limit:
+            periodic_split.append([start, end])
+        else:
+            periodic_split += split_segments(start, end, periodic_limit)
+    segmentLimits = periodic_split
+    # print("segmentLimits:", segmentLimits)
+
+    ### process each segment
+    transcriptions = []
+    # for start, end in segmentLimits:
+    for start, end in segmentLimits:
+        split_audio, sr = librosa.load(input_filepath, sr=16000, offset=start, duration=end - start)
+
+        # https://pypi.org/project/noisereduce/#:~:text=Noise%20reduction%20in%20python%20using,a%20form%20of%20Noise%20Gate.
+        ### noisereduce
+        noise_reduced = nr.reduce_noise(y=split_audio, sr=sr)
+
+        ### run model
+        input_values = tokenizer(noise_reduced, return_tensors="pt").input_values
+        logits = model(input_values).logits
+        predicted_ids = torch.argmax(logits, dim=-1)
+        t = tokenizer.batch_decode(predicted_ids)[0]
+
+        # https://stackoverflow.com/questions/54587178/python-capitalize-first-character-of-words-in-a-string-except-one-word
+        ### lowercase all letters except for I
+        t = " ".join([word.lower() if word.lower() != 'i' else word for word in t.split()])
+        ### capitalize
+        t = t.capitalize()
+
+        # print("transcription:", t)
+        transcriptions.append(t)
+
+    ### write transcriptions into output srt file
+    # https://docs.fileformat.com/video/srt/
+    try:
+        with open(output_filepath, 'w') as f:
+            ### one for each segment
+            for t in range(len(transcriptions)):
+                f.write(str(t+1))
+                f.write('\n')
+                ### convert segment limits from seconds to hours:minutes:seconds,milliseconds 00:00:00,000 format
+                f.write(convert_time_format(segmentLimits[t][0]) + ' --> ' + convert_time_format(segmentLimits[t][1]))
+                f.write('\n')
+                ### split segments into multiple lines based on character limit per line
+                segment = transcriptions[t]
+                lines = split_phrases(segment, char_limit)
+                for line in lines:
+                    f.write(line) 
+                    f.write('\n')
+                f.write('\n')
+    except FileNotFoundError:
+        print('Unable to find output file path:', output_filepath)
+        sys.exit()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
