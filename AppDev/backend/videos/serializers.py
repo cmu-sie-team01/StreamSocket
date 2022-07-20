88ed95882140b44fd5cdef61ec7dc4eef15ea6b6
Yuean Zhao <1019948735@qq.com>
2022-07-19 18:14:38 -0700

    backend: 1. small fix

--- AppDev/backend/videos/serializers.py
+++ AppDev/backend/videos/serializers.py
@@ -6,7 +6,7 @@ class VideoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Video
-        fields = ['author', 'video', 'likesCount', 'updated', 'created', 'caption']
+        fields = ['author', 'video', 'likesCount', 'updated', 'created', 'caption', 'id']
         extra_kwargs = {
             'likesCount': {'read_only': True},
         }
