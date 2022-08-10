2566156134174ad00420e0961522f0728b9d6e1d
DHX98 <haox.dai@gmail.com>
2022-08-10 00:25:04 -0700

    -Remove chat feature

--- AppDev/frontend/src/component/DirectMsg.jsx
+++ AppDev/frontend/src/component/DirectMsg.jsx
@@ -1,25 +1,10 @@
 import * as React from 'react';
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
-import Draggable from 'react-draggable';
 import MailIcon from '@mui/icons-material/Mail';
 import {
-  Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
+  Dialog,
 } from '@mui/material';
-import Paper from '@mui/material/Paper';
-
-function PaperComponent(props) {
-  return (
-    <Draggable
-      handle="#draggable-dialog-title"
-      cancel={'[class*="MuiDialogContent-root"]'}
-    >
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Paper {...props} />
-    </Draggable>
-  );
-}
 
 export default function DirectMsg() {
   const [open, setOpen] = React.useState(false);
@@ -49,25 +34,9 @@ export default function DirectMsg() {
         <Dialog
           open={open}
           onClose={handleClose}
-          PaperComponent={PaperComponent}
           aria-labelledby="draggable-dialog-title"
         >
-          <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-            Direct messages
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              chat 1
-            </DialogContentText>
-            <DialogContentText>
-              chat 2
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
-              Dismiss
-            </Button>
-          </DialogActions>
+          <div style={{ margin: '5%' }}>This feature is still Under construction</div>
         </Dialog>
       </div>
     </div>
